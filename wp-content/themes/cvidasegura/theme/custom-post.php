a1161698a1d54139d2bfdb7372728954175e2633
vdavidmarques <vdavidmarques@gmail.com>
2024-04-26 21:51:55 -0300

    chor: Adding taxonomies to custom_post planos-de-saude

--- wp-content/themes/cvidasegura/theme/custom-post.php
+++ wp-content/themes/cvidasegura/theme/custom-post.php
@@ -75,11 +75,37 @@
         );
         
         register_post_type( 'planos-de-saude', $args );
+
+         // Registrar Taxonomia para Planos de Saúde
+         $labels_taxonomy = array(
+            'name'              => 'Categorias de Planos de Saúde',
+            'singular_name'     => 'Categoria de Planos de Saúde',
+            'search_items'      => 'Pesquisar Categorias',
+            'all_items'         => 'Todas as Categorias',
+            'parent_item'       => 'Categoria Pai',
+            'parent_item_colon' => 'Categoria Pai:',
+            'edit_item'         => 'Editar Categoria',
+            'update_item'       => 'Atualizar Categoria',
+            'add_new_item'      => 'Adicionar Nova Categoria',
+            'new_item_name'     => 'Novo Nome de Categoria',
+            'menu_name'         => 'Categorias de Planos de Saúde',
+        );
+        
+        $args_taxonomy = array(
+            'hierarchical'      => true,
+            'labels'            => $labels_taxonomy,
+            'show_ui'           => true,
+            'show_admin_column' => true,
+            'query_var'         => true,
+            'rewrite'           => array( 'slug' => 'categoria-planos-de-saude' ),
+        );
+        
+        register_taxonomy( 'categoria_planos-de-saude', 'planos-de-saude', $args_taxonomy );
     }
     add_action( 'init', 'registrar_planos_saude_custom_post' );
 
     // Registrar Custom Post Type Seguros
-    function registrar_seguros_vida_custom_post() {
+    function registrar_seguros_custom_post() {
         $labels = array(
             'name'               => 'Seguros',
             'singular_name'      => 'Seguro',
@@ -109,13 +135,40 @@
             'has_archive'        => true,
             'hierarchical'       => false,
             'menu_position'      => null,
-            'supports'           => array( 'title', 'editor', 'thumbnail', 'custom-fields'),
-            'menu_icon'          => 'dashicons-shield' //Seguros
+            'supports'           => array( 'title', 'editor','thumbnail', 'custom-fields' ),
+            'menu_icon'          => 'dashicons-shield'
         );
         
         register_post_type( 'seguros', $args );
+
+        // Registrar Taxonomia para Seguros
+        $labels_taxonomy = array(
+            'name'              => 'Categorias de Seguros',
+            'singular_name'     => 'Categoria de Seguro',
+            'search_items'      => 'Pesquisar Categorias',
+            'all_items'         => 'Todas as Categorias',
+            'parent_item'       => 'Categoria Pai',
+            'parent_item_colon' => 'Categoria Pai:',
+            'edit_item'         => 'Editar Categoria',
+            'update_item'       => 'Atualizar Categoria',
+            'add_new_item'      => 'Adicionar Nova Categoria',
+            'new_item_name'     => 'Novo Nome de Categoria',
+            'menu_name'         => 'Categorias de Seguros',
+        );
+        
+        $args_taxonomy = array(
+            'hierarchical'      => true,
+            'labels'            => $labels_taxonomy,
+            'show_ui'           => true,
+            'show_admin_column' => true,
+            'query_var'         => true,
+            'rewrite'           => array( 'slug' => 'categoria-seguro' ),
+        );
+        
+        register_taxonomy( 'categoria_seguro', 'seguros', $args_taxonomy );
     }
-    add_action( 'init', 'registrar_seguros_vida_custom_post' );
+    add_action( 'init', 'registrar_seguros_custom_post' );
+
 
     // Registrar Custom Post Type Planos Odontológicos
     function registrar_planos_odontologicos_custom_post() {
