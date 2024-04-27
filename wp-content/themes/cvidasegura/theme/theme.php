957f3c091b42f0bdccff00f87dec8e72d2ac9087
vdavidmarques <vdavidmarques@gmail.com>
2024-04-26 21:49:45 -0300

    chore: Change tailwindcss to sass cause wasnt working well

--- wp-content/themes/cvidasegura/theme/theme.php
+++ wp-content/themes/cvidasegura/theme/theme.php
@@ -1,12 +1,15 @@
 <?php
-        
+    /*******************************
+    Adding scripts and Css
+        ********************************/   
     add_action( 'wp_enqueue_scripts', function () {
         if (!is_admin()) {
+            wp_enqueue_style( 'style', get_template_directory_uri() . './style.css', array(), '1.0.0' );
             
-            wp_enqueue_style( 'output', get_template_directory_uri() . '/src/css/output.css', array(), '1.0.0' );
-		    wp_enqueue_script( 'script-js', get_template_directory_uri() . "/library/js/scripts.min.js", array( 'jquery' ), null, true );
-        
+            wp_enqueue_script( 'script-js', get_template_directory_uri() . "/library/js/scripts.min.js", array( 'jquery' ), null, true );
+
+            wp_enqueue_script( 'swiper', get_template_directory_uri() . "/library/js/swiper.min.js", array( 'jquery' ), null, true );            
         }
-    });
+    }); 
 
 ?>
