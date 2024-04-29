59d37ad96127c40b78d5de2280d00ad354ba74de
vdavidmarques <vdavidmarques@gmail.com>
2024-04-29 18:04:22 -0300

    chore: Updating our-service componet - style e swiper

--- wp-content/themes/cvidasegura/blocks/service-image.php
+++ wp-content/themes/cvidasegura/blocks/service-image.php
@@ -1,24 +1,71 @@
-<?php
-    $blockOne = get_field('our-services-service-image', '63');//Primeiro bloco
-        $firstServiceBlockOne = $blockOne['our-services-first-service'];//Primeiro Serviço
-            $titleFirstServiceBlockOne = $firstServiceBlockOne['title'];
-            $descriptionFirstServiceBlockOne = $firstServiceBlockOne['description'];
-            $linkFirstServiceBlockOne = $firstServiceBlockOne['link']['url'];
-        $secondServiceBlockOne = $blockOne['our-services-second-service'];//Segundo Serviço
-            $titleSecondServiceBlockOne = $secondServiceBlockOne['title'];
-            $descriptionSecondServiceBlockOne = $secondServiceBlockOne['description'];
-            $linkSecondServiceBlockOne = $secondServiceBlockOne['link']['url'];
-        $imageFirstServiceBlockOne =  $blockOne['image'];
- ?>
 <div class="service-image">
-    <p itemprop="articleBody">
+    <?php
+        $blockOne = get_field('our-services-service-image', '63');       
+        $services = $blockOne['our-services-service']; 
+    ?>
+  
+    <div class="images image-1">
+        <?php if($blockOne['image']): ?>
+            <img src="<?php echo $blockOne['image']['url']; ?>" alt="<?php echo $blockOne['image']['alt']; ?>" itemprop="image" class="">
+        <?php endif; ?>
+    </div>
+
+    <div class="services-cards swiper-container">
+        <div class="swiper-wrapper">
+            <?php 
+                if($services):
+                    foreach( $services as $service ) : 
+                ?>   
+                
+                    <article class="service-1 card swiper-slide" itemscope itemprop="articleBody">
+                        <?php 
+                            if($service['link-option']) :  
+                        
+                                $args = array(
+                                    'name' => 'informacoes',
+                                    'post_type' => 'page',
+                                );
+                
+                                $query = new WP_Query($args);
+                                while ($query->have_posts()) :
+                                $query->the_post();
+                                $whatsapp = get_field('whatsapp');
+                                $whatsappNumber = get_field('whatsappNumber');
+                                $whatsappMessage = get_field('whatsappMessage');
+                        ?>
+
+                                <a target="_blank" href="https://api.whatsapp.com/send?phone=<?php echo $whatsappNumber ?>&text=<?php echo $whatsappMessage ?>">
+                                    <span class="header">
+                                        <img src="<?php echo $service['icone'] ?>" alt="<?php echo $service['texto'] ?>"  itemprop="image" class="icon">
+                                        <h2 class="title"><?php echo $service['title'] ?></h2>
+                                    </span>
+                                    <p class="text"><?php echo $service['texto'] ?></p>
+                                </a> 
+                
+                            <?php 
+                                endwhile;
+                                else :  
+                            ?>
+                                <a target="_blank" href="<?php echo $service['link']['url'] ; ?>">
+                                    <span class="header">
+                                        <img src="<?php echo $service['icone'] ?>" alt="<?php echo $service['texto'] ?>"  itemprop="image" class="icon">
+                                        <h2 class="title"><?php echo $service['title'] ?></h2>
+                                    </span>
+                                    <p class="text"><?php echo $service['texto'] ?></p>
+                                </a> 
+                        <?php endif; ?>
+                    </article>
+                <?php 
+                    endforeach;
+                endif;
+            ?>
+        </div>
+    </div>
+
     
-    <?php echo $titleFirstServiceBlockOne ?>
-    <?php echo $descriptionFirstServiceBlockOne ?>
-    <?php echo $linkFirstServiceBlockOne ?>
-    <?php echo $titleSecondServiceBlockOne ?>
-    <?php echo $descriptionSecondServiceBlockOne ?>
-    <?php echo $linkSecondServiceBlockOne ?>
-    </p>
-    <img src="<?php echo $imageFirstServiceBlockOne ?>" alt="<?php echo $titleFirstServiceBlockOne ?>" itemprop="image">
+    <div class="images show-desktop image-2">
+        <?php   if($blockOne['image-2']): ?>
+            <img src="<?php echo $blockOne['image-2']['url']; ?>" alt="<?php echo $blockOne['image-2']['alt']; ?>" itemprop="image">
+        <?php endif; ?>
+    </div>   
 </div>
--- wp-content/themes/cvidasegura/components/our-services.php
+++ wp-content/themes/cvidasegura/components/our-services.php
@@ -1,14 +1,14 @@
-<div class="our-services py-20 mt-20 lg:mt-[12.5rem]">
+<section class="our-services py-20 mt-20 lg:mt-[12.5rem]" itemscope itemtype="http://schema.org/Article">
     <?php
         $introTitle = get_field("intro-main-title","63");
         $ourServiceMainTitle = get_field("our-services-main-title", "63");
-        $ourDervicesDescription = get_field('our-services-description', 63);
+        $ourDervicesDescription = get_field('our-services-description', 63);      
     ?>
-
-    <h1 itemprop="headline">Consultoria Vida Segura - <?php echo $introTitle ?></h1>
-    <h2 itemprop="headline"><?php echo $ourServiceMainTitle ?></h2>
-    <h3 itemprop="headline"><?php echo $ourDervicesDescription ?> </h3>
-
+    <div class="container content-text grid grid-cols-12">
+        <div class="wrap">
+            <div class="title"><?php echo $ourServiceMainTitle ?></div>
+            <p itemprop="headline" class="text"><?php echo $ourDervicesDescription ?> </p>
+        </div>
+    </div>
     <?php include get_template_directory() . '/blocks/service-image.php'; ?>
-    <?php include get_template_directory() . '/blocks/image-service.php';  ?>
-</div>
+</section>
