85698bcff34080eb5761857e936bd785dac578cc
vdavidmarques <vdavidmarques@gmail.com>
2024-04-29 23:07:52 -0300

    chore: Updating we-experts component style

--- wp-content/themes/cvidasegura/components/we-experts.php
+++ wp-content/themes/cvidasegura/components/we-experts.php
@@ -1,17 +1,35 @@
-<article class="we-experts py-20" itemscope itemprop="articleBody">
+<article class="we-experts" itemscope itemprop="articleBody">
     <?php
-        $bgImage = get_field("we-experts-image", "63");
-        $logo = get_field("we-experts-logo", "63");
-        $title = get_field("we-experts-title", "63");
-        $text = get_field("we-experts-text", "63");
-        $wppLabelButton = get_field("we-experts-wpp-label", "63");
-        $emailLabelButton = get_field("we-experts-email-label", "63");
+    $bgImage = get_field("we-experts-image", "63");
+    $title = get_field("we-experts-title", "63");
+    $wppLabelButton = get_field("we-experts-wpp-label", "63");
     ?>
 
-    <?php echo $bgImage ?>
-    <?php echo $logo ?>
-    <p itemprop="articleBody"><?php echo $title ?> </p>
-    <p itemprop="articleBody"><?php echo $text ?> </p>
-    <p itemprop="articleBody"><?php echo $wppLabelButton ?> </p>
-    <p itemprop="articleBody"><?php echo $emailLabelButton ?> </p>
+    <div class="blue-bg">&nbsp;</div>
+
+    <div class="grid grid-cols-12">
+        <div class="bg-image">
+            <img src="<?php echo $bgImage ?>" alt="Consultoria Vida Segura: Protegendo o que Realmente Importa" itemprop="image" class="bg-image">
+        </div>
+        <div class="content">
+            <div itemprop="articleBody" class="default-heading-title"><?php echo $title ?> </div>
+            <?php
+            $args = array(
+                'name' => 'informacoes',
+                'post_type' => 'page',
+            );
+
+            $query = new WP_Query($args);
+            while ($query->have_posts()) {
+                $query->the_post();
+                $whatsapp = get_field('whatsapp');
+                $whatsappNumber = get_field('whatsappNumber');
+                $whatsappMessage = get_field('whatsappMessage');
+            ?>
+                <a target="_blank" href="https://api.whatsapp.com/send?phone=<?php echo $whatsappNumber ?>&text=<?php echo $whatsappMessage ?>" class="button-first">
+                    <?php echo $wppLabelButton ?>
+                </a>
+            <?php } ?>
+        </div>
+    </div>
 </article>
