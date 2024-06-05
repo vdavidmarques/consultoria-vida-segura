a67ac5cd2840331e78cb5d18712f7331b75e2663
vdavidmarques <vdavidmarques@gmail.com>
2024-06-05 19:21:33 -0300

    feature: Adding policy-privacy page

---
+++ wp-content/themes/cvidasegura/page-politica-de-privacidade.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+<?php include 'blocks/breadcrumb.php' ?>
+    <section class="page-politica-de-privacidade container mt-20">
+        <article class="default-heading-title">
+            <?php echo get_field('privacy-policy-text', '3') ?>
+        </article>
+    </section>
+<?php get_footer(); ?>
