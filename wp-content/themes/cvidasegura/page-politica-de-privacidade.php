254414ba8b6129f014845ea98e86deea2984de74
vdavidmarques <vdavidmarques@gmail.com>
2024-08-08 22:52:49 -0300

    chore: Updating page-policy-privacy style

--- wp-content/themes/cvidasegura/page-politica-de-privacidade.php
+++ wp-content/themes/cvidasegura/page-politica-de-privacidade.php
@@ -2,7 +2,7 @@
 <?php include 'blocks/breadcrumb.php' ?>
     <section class="page-politica-de-privacidade container mt-20">
         <article class="default-heading-title">
-            <?php echo get_field('privacy-policy-text', '3') ?>
+            <?php echo get_field('privacy-policy-text', '2627') ?>
         </article>
     </section>
 <?php get_footer(); ?>
