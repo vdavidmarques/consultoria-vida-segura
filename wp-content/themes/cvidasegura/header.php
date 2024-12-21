3612f69f149f5c66115a595cb31c61431031e374
vdavidmarques <vdavidmarques@gmail.com>
2024-12-20 22:35:28 -0300

    feat: Adding cannonical link

--- wp-content/themes/cvidasegura/header.php
+++ wp-content/themes/cvidasegura/header.php
@@ -8,39 +8,47 @@
     <?php wp_head(); ?>
     <title itemprop="name">
         <?php
-            if(is_home()){
+        switch (true) {
+            case is_home():
                 echo get_the_title(63) . ' | ';
-            }elseif(is_tax()){
+                break;
+            case is_tax():
                 echo single_cat_title() . ' | ';
-            }elseif(is_archive()){
+                break;
+            case is_archive():
                 echo get_the_archive_title() . ' | ';
-            }elseif(is_singular()){
+                break;
+            case is_singular():
                 echo single_post_title() . ' | ';
-            }else{
+                break;
+            default:
                 echo get_the_title() . ' | ';
-            }
-            echo 'Consultoria Vida Segura';
+                break;
+        }
+        echo 'Consultoria Vida Segura';
         ?>
     </title>
+
     <link itemprop="url" href="https://www.consultoriavidasegura.com">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+    <link rel="canonical" href="https://consultoriavidasegura.com/">
 
 </head>
 
-<body <?php body_class(); ?>>   
+<body <?php body_class(); ?>>
     <header itemscope itemtype="http://schema.org/WPHeader">
         <div class="header-top">
             <div class="socials-and-menu">
-                <?php 
-                    require_once 'services/social-icons-display.php';
-                    $socialIcons = new SocialIconsDisplay(['whatsapp', 'phone', 'email', 'instagram', 'facebook']);
-                    $socialIcons->display_social_icons();
+                <?php
+                require_once 'services/social-icons-display.php';
+                $socialIcons = new SocialIconsDisplay(['whatsapp', 'phone', 'email', 'instagram', 'facebook']);
+                $socialIcons->display_social_icons();
                 ?>
             </div>
         </div>
         <div class="header-menu-itens container">
-            <a href="/"> 
+            <a href="/">
                 <img src="<?php echo  get_template_directory_uri() . '/dist/library/images/consultoria-vida-segura-logo.webp' ?>" alt="Consultoria Vida Segura - Empresa especializa em Planos de Saúde, odontológicos e funeral e Seguros de vida" class="logo" itemprop="image">
             </a>
 
@@ -53,5 +61,5 @@
             </div>
 
         </div>
-     </header>
-        <main itemprop="mainContentOfPage">
+    </header>
+    <main itemprop="mainContentOfPage">
--- wp-content/themes/cvidasegura/home.php
+++ wp-content/themes/cvidasegura/home.php
@@ -7,7 +7,7 @@ include_once 'services/check-enviroment.php';
     include 'components/main-banner.php';
     include 'components/our-services.php';
      include 'components/we-experts.php';
-    include 'components/depositions.php';
+    //include 'components/depositions.php';
     ?>
 </div>
 <?php get_footer(); ?>
